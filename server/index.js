f3c0be818c799ebd237b0a78820609baa0ffe9ce
Arunesh Pratap Singh Tomar <98048674+TheDarkknight26@users.noreply.github.com>
2023-10-01 02:25:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin:["https://deploy-mern-1whq.vercel.app"],
+  origin:["https://bookfdfrontend.vercel.app"],
   methods:["POST","GET"],
   credentials:true
 }
