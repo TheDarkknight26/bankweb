1ca03c42302527b7ce304ac2b691d29b9fb069f8
Arunesh Pratap Singh Tomar <98048674+TheDarkknight26@users.noreply.github.com>
2023-10-01 06:26:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://frontendbookfd.vercel.app", // Allow requests from all origins
+  origin: "https://banksdatahostingfrontend.vercel.app", // Allow requests from all origins
   methods: ["POST", "GET"],
   credentials: true,
 }));
