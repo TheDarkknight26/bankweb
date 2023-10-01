40b9b7e73c4f6e5909683304f8a66419f5bd8f1b
Arunesh Pratap Singh Tomar <98048674+TheDarkknight26@users.noreply.github.com>
2023-10-01 06:17:54 +0530

    Update fddata.jsx

--- client/src/components/FDdata/fddata.jsx
+++ client/src/components/FDdata/fddata.jsx
@@ -46,7 +46,7 @@ const Fdrates = () =>{
 
     const fetchBankfddata = async () => {
         try {
-          const response = await axios.get("https://banksdatahosting.vercel.app/fdrates", {
+          const response = await axios.get("/fdrates", {
             params: {
               bankNames: JSON.stringify(banks), // Send the banks array as a parameter
             },
