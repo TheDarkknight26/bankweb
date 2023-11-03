a1ffdcc39ceca1d003dee7685c92c942b873e820
Arunesh Pratap Singh <aruneshpratapsingh2003@gmail.com>
2023-11-04 01:07:33 +0530

    changes

--- client/src/components/AboutUs/About.js
+++ client/src/components/AboutUs/About.js
@@ -7,8 +7,8 @@ const AboutUsPage = () => {
             <div className={styles["about-us-content"]}>
                 <h2>About Us</h2>
                 <p>
-We extend a warm welcome to our website, where our foremost commitment is delivering exceptional service to our esteemed users.</p>
-                <p> Initiated by Professor Manish Kumar Singh, a distinguished member of the Economics faculty at the prestigious Indian Institute of Technology, Roorkee, the concept for this platform was brought to life through by Arunesh Pratap Singh Tomar and Anish Kumar. The vision was to create a resource that empowers individuals to make well-informed decisions regarding the selection of the most suitable fixed deposit schemes offered by a variety of banks. With a core focus on enabling users to optimize their financial returns and realize their aspirations with confidence and reliability, our mission is to foster a secure and knowledgeable financial environment for all.</p>
+                We extend a warm welcome to our website, where our foremost commitment is delivering exceptional service to our esteemed users.</p>
+                <p> Initiated by Professor Manish Kumar Singh, a distinguished member of the Economics faculty at the prestigious Indian Institute of Technology, Roorkee, the concept for this platform was brought to life by Arunesh Pratap Singh Tomar and Anish Kumar. The vision was to create a resource that empowers individuals to make well-informed decisions regarding the selection of the most suitable fixed deposit schemes offered by a variety of banks. With a core focus on enabling users to optimize their financial returns and realize their aspirations with confidence and reliability, our mission is to foster a secure and knowledgeable financial environment for all.</p>
             </div>
         </div>
     );
