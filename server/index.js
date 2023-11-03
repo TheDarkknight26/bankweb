00d72ad1e628842ecc0c62d6249be9e1adcf7352
Arunesh Pratap Singh <aruneshpratapsingh2003@gmail.com>
2023-11-04 00:09:03 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -21,27 +21,26 @@ app.use(cors());
 
 const PORT=process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname,'../client/build')));
-
+// app.use(express.static(path.join(__dirname,'../client/build')));
 
 
 app.use("/result",bankrouter);
 app.use("/resultbtwdates",bankrouternew);
 app.use("/fdrates",fdratesrouter);
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname,'../client/build')));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../client/build')));
    
-//     app.get('*',(req,res)=>
-//         res.sendFile(
-//             path.resolve(__dirname,'../','client','build','index.html')
-//         )
-//     );
-// } else {
-//     app.get('/',(req,res)=> res.send('Please set to production')
-//     );
+    app.get('*',(req,res)=>
+        res.sendFile(
+            path.resolve(__dirname,'../','client','build','index.html')
+        )
+    );
+} else {
+    app.get('/',(req,res)=> res.send('Please set to production')
+    );
   
-// }
+}
 
 app.listen(PORT,()=>{
     console.log(`server running fine at ${PORT}`);
